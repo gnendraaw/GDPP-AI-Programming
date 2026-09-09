7583e397550c3511a3c425edca2a1d8d1be26cbe
Ardell <itsgnendraaw@gmail.com>
2026-09-10 03:09:24 +0800

    Setup SightPerception

---
+++ Assets/Game/Scripts/Ghost/AI/SightPerception.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public class SightPerception : MonoBehaviour
+{
+    [SerializeField] private Transform _eyePosition;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _viewDistance = 10f;
+    [SerializeField] private float _viewAngle = 70f;
+    [SerializeField] private LayerMask _targetLayer;
+    
+    public bool CanSeePlayer { get; private set; }
+    public Vector3 LastSeenPosition { get; private set; }
+
+    private void Update()
+    {
+        CanSeePlayer = CheckSight();
+    }
+
+    private bool CheckSight()
+    {
+        if (_target == null)
+            return false;
+        
+        float distance = Vector3.Distance(_eyePosition.position, _target.position);
+        if (distance > _viewDistance)
+            return false;
+        
+        Vector3 directionToTarget = _target.position - _eyePosition.position;
+        float angle = Vector3.Angle(_eyePosition.forward, directionToTarget);
+        if (angle > _viewAngle * 0.5f)
+            return false;
+
+        bool isSeeingTarget = Physics.Raycast(
+            _eyePosition.position,
+            directionToTarget.normalized,
+            out RaycastHit hit,
+            _viewDistance,
+            _targetLayer);
+        if (!isSeeingTarget) return false;
+
+        if (hit.transform == _target)
+        {
+            LastSeenPosition = _target.position;
+            return true;
+        }
+        
+        return false;
+    }
+}
